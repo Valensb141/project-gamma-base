1dec139793367d7689a534f9e143e010e9626b73
Valensb141 <116755122+Valensb141@users.noreply.github.com>
2023-05-03 19:38:18 +0200

    agregue los morados al style guide

--- src/styleguide/Styleguide.jsx
+++ src/styleguide/Styleguide.jsx
@@ -3,14 +3,23 @@ import './Styleguide.scss'
 
 // Tambn importar los componentes que se usarán
 import Nav from './components/Nav/Nav'
-//import Btn from './components/btn/Btn'
+import Btn from './components/btn/Btn'
 
 
 // 2. Crear la función 
 function Styleguide() {
     return <>
-        <Nav />
-        <h1>SOY LA STYLEGUIDE</h1>
+        {/* <Nav />
+        <h1>SOY LA STYLEGUIDE</h1> */}
+
+        <h2>Colors</h2>
+        <div className='colors__container'>
+            <div className="bg-color-darkerPurple"></div>
+            <div className="bg-color-darkPurple"></div>
+            <div className="bg-color-mediumPurple"></div>
+            <div className="bg-color-lightPurple"></div>
+            <div className="bg-color-lighterPurple"></div>
+        </div>
     </>
 }
 
