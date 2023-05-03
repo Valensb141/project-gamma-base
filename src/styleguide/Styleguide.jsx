2efdc04971c32b060ba095c946717059fca84170
Valensb141 <116755122+Valensb141@users.noreply.github.com>
2023-05-03 21:37:32 +0200

    variables added

--- src/styleguide/Styleguide.jsx
+++ src/styleguide/Styleguide.jsx
@@ -9,16 +9,26 @@ import Btn from './components/btn/Btn'
 // 2. Crear la función 
 function Styleguide() {
     return <>
-        {/* <Nav />
-        <h1>SOY LA STYLEGUIDE</h1> */}
+
+        <h1>Valentina's Styleguide</h1> 
 
         <h2>Colors</h2>
         <div className='colors__container'>
-            <div className="bg-color-darkerPurple"></div>
-            <div className="bg-color-darkPurple"></div>
-            <div className="bg-color-mediumPurple"></div>
-            <div className="bg-color-lightPurple"></div>
-            <div className="bg-color-lighterPurple"></div>
+            <div className='colors__subcontainer'>
+                <div className="bg-color-darkerPurple"></div>
+                <div className="bg-color-darkPurple"></div>
+                <div className="bg-color-mediumPurple"></div>
+                <div className="bg-color-lightPurple"></div>
+                <div className="bg-color-lighterPurple"></div>
+            </div>
+
+            <div className='colors__subcontainer'>
+                <div className="bg-color-darkerGreen"></div>
+                <div className="bg-color-darkGreen"></div>
+                <div className="bg-color-mediumGreen"></div>
+                <div className="bg-color-lightGreen"></div>
+                <div className="bg-color-lighterGreen"></div>
+            </div>
         </div>
     </>
 }
