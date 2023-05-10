4bd9f45708a52d0b74821f3b732d7e9f7f7a48ce
Valensb141 <116755122+Valensb141@users.noreply.github.com>
2023-05-10 20:52:00 +0200

    added typography classes

--- src/styleguide/Styleguide.jsx
+++ src/styleguide/Styleguide.jsx
@@ -12,23 +12,62 @@ function Styleguide() {
 
         <h1>Valentina's Styleguide</h1> 
 
-        <h2>Colors</h2>
+        
         <div className='colors__container'>
-            <div className='colors__subcontainer'>
-                <div className="bg-color-darkerPurple"></div>
-                <div className="bg-color-darkPurple"></div>
-                <div className="bg-color-mediumPurple"></div>
-                <div className="bg-color-lightPurple"></div>
-                <div className="bg-color-lighterPurple"></div>
-            </div>
+            <h2>Colors</h2>
+                <div className='colors__subcontainer'>
+                    <div className="bg-color-darkerPurple"></div>
+                    <div className="bg-color-darkPurple"></div>
+                    <div className="bg-color-mediumPurple"></div>
+                    <div className="bg-color-lightPurple"></div>
+                    <div className="bg-color-lighterPurple"></div>
+                </div>
 
-            <div className='colors__subcontainer'>
-                <div className="bg-color-darkerGreen"></div>
-                <div className="bg-color-darkGreen"></div>
-                <div className="bg-color-mediumGreen"></div>
-                <div className="bg-color-lightGreen"></div>
-                <div className="bg-color-lighterGreen"></div>
-            </div>
+                <div className='colors__subcontainer'>
+                    <div className="bg-color-darkerGreen"></div>
+                    <div className="bg-color-darkGreen"></div>
+                    <div className="bg-color-mediumGreen"></div>
+                    <div className="bg-color-lightGreen"></div>
+                    <div className="bg-color-lighterGreen"></div>
+                </div>
+
+                <div className='colors__subcontainer'>
+                    <div className="bg-color-darkerGreen"></div>
+                    <div className="bg-color-darkGreen"></div>
+                    <div className="bg-color-mediumGreen"></div>
+                    <div className="bg-color-lightGreen"></div>
+                    <div className="bg-color-lighterGreen"></div>
+                </div>
+
+                <div className='colors__subcontainer'>
+                    <div className="bg-color-darkerGreen"></div>
+                    <div className="bg-color-darkGreen"></div>
+                    <div className="bg-color-mediumGreen"></div>
+                    <div className="bg-color-lightGreen"></div>
+                    <div className="bg-color-lighterGreen"></div>
+                </div>
+
+                <div className='colors__subcontainer'>
+                    <div className="bg-color-darkerGreen"></div>
+                    <div className="bg-color-darkGreen"></div>
+                    <div className="bg-color-mediumGreen"></div>
+                    <div className="bg-color-lightGreen"></div>
+                    <div className="bg-color-lighterGreen"></div>
+                </div>
+        </div>
+
+
+        
+        <div className="colors__container">
+            <h2>Typography</h2>
+                <div>
+                    <h1 className='h1'>H1 - Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h1>
+                    <h2>H2 - Lorem ipsum dolor sit amet, consectetur adipiscing elit</h2>
+                    <h3>H3 - Lorem ipsum dolor sit amet, consectetur adipiscing elit</h3>
+                    <h4>H4 - Lorem ipsum dolor sit amet, consectetur adipiscing elit</h4>
+                    <p>p - Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                </div>
+
         </div>
     </>
 }
